bb187be6e7eab52cf80529743d929e0af87d877e
Caleb Hendren <109324308+CalebHendren@users.noreply.github.com>
2023-03-22 02:02:03 -0400

    Update Metadata.py

--- Metadata.py
+++ Metadata.py
@@ -2,7 +2,6 @@ from Bio import Entrez
 import xml.etree.ElementTree as ET
 Entrez.email = "calebahendren@gmail.com"
 
-
 def get_sra_accessions(query):
     search_handle = Entrez.esearch(db="sra", term=query, retmax=1000)
     search_results = Entrez.read(search_handle)
@@ -21,7 +20,7 @@ project_id = "PRJNA839090"
 accessions = get_sra_accessions(f"{project_id}[Project] AND Tennessee[All Fields]")
 print(f"Found {len(accessions)} SRA accessions for project {project_id} in Tennessee:")
 
-#Save accessions to a text file
+#Save accessions to a text file 
 with open("tennessee_sra_accessions.txt", "w") as accession_file:
     for acc in accessions:
         accession_file.write(f"{acc}\n")
